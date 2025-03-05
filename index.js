f05abac54a770eab696ebe1697b276114483c025
KYANTECH254 <kyantech254@gmail.com>
2025-03-05 12:56:59 -0500

    update

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-const mikrotikRoutes = require("./mikrotik/routes/mikrotikRoutes");
+const path = require("path");
+// const mikrotikRoutes = require("./mikrotik/routes/mikrotikRoutes");
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/mkt", mikrotikRoutes);
+app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = process.env.PORT || 5000;
+// app.use("/api/mkt", mikrotikRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on https://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
